7beba6779a25f4ed6acb7e87f1f590702c976eac
jocvegar <jocvegar@gmail.com>
2020-01-26 22:22:26 -0600

    Adds bootstrap, jquery, navbar, footer, carousel

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,19 @@
 const { environment } = require('@rails/webpacker')
 
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+);
+
+const aliasConfig = {
+	'jquery': 'jquery/dist/jquery.min.js',
+	$: 'jquery/dist/jquery.min.js',
+};
+
+environment.config.set('resolve.alias', aliasConfig);
+
 module.exports = environment
